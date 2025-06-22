3290fcd1298534094651ebf37ad1433d1161b647
1411swetha <aswetha.1411@gmail.com>
2025-06-22 10:31:26 +0000

    set example

---
+++ Sets/set.py
@@ -0,0 +1,9 @@
+#set example
+s3_names = {"s3_abc", "s3_xyz", "s3_qwe"}
+for name in s3_names:
+    print(name)
+s3_names.add("s3_abc")
+
+# list = ["s3_abc", "s3_xyz", "s3_qwe"]
+# n = list[1]
+# print(n)
